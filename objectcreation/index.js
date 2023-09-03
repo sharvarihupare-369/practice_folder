3b4de7d90c8b1ef90268aa846ed894558bef4f3c
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-09-03 21:42:56 +0530

    add

---
+++ objectcreation/index.js
@@ -0,0 +1,27 @@
+//#1 object literal ==> 
+let user = {
+    name : "John",
+    age : 23
+}
+
+//#2 object constructor ==> 
+
+let user1 = new Object({
+    name  : "Jane",
+    age : 20
+})
+
+// constructors are special functions that when used with new  keyword which creates  object
+
+//#3 Object.create method to create new object
+let user2 = Object.create({
+    name : "Bob",
+    age  : 40
+})
+
+console.log(user)
+console.log(user1)
+console.log(user2)
+
+// console.log(user === user2)
+// console.log(user === user1)
